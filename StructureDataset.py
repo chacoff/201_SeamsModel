3a691e94db13d96a89e82c1e01995adb21a03959
Gomez Chacoff, Jaime <Jaime.Gomez-Chacoff@arcelormittal.com>
2023-01-30 23:35:34 +0100

    dataset, tools update

--- StructureDataset.py
+++ StructureDataset.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+import glob
 
 
 def list_files(startpath):
@@ -13,4 +15,23 @@ def list_files(startpath):
     sourceFile.close()
 
 
-list_files('C:\\Coding\\201_SeamsModel\\images')
+def find_and_move(base, file, target):
+    file_noextension = file.split('.')[0]
+    result = glob.glob(f'{base}/**/{file_noextension}.bmp', recursive=True)  # a list, i take the first element
+    final_target = f'{target}\\{file_noextension}.bmp'
+    shutil.move(result[0], final_target)
+    print(f'moved: {file_noextension}.bmp')
+
+
+# list_files('C:\\Coding\\201_SeamsModel\\images')
+
+base = 'C:\\Users\\gomezja\\PycharmProjects\\Tmb\\00_JG-SB_Validation01_2022_02_11-28'
+target = 'C:\\Users\\gomezja\\PycharmProjects\\201_SeamsModel\\images\\valid\\Seams'
+
+file1 = open('images\\valid_seams.txt', 'r')
+Lines = file1.readlines()
+
+for line in Lines:
+    l = format(line.strip())
+    find_and_move(base, l, target)
+
