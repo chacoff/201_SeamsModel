cfb008f2d04fa5831a3ce83bbb984a77424a45a7
chacoff <jaime.arctico@gmail.com>
2023-02-01 00:45:09 +0100

    csv with new annotations and hyper parameters

--- detect.py
+++ detect.py
@@ -149,11 +149,11 @@ def detect(save_img=False):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='runs/train/exp20/weights/best.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='images/test/Seams', help='source')  # file/folder, 0 for webcam
-    parser.add_argument('--img-size', type=int, default=736, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.10, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.25, help='IOU threshold for NMS')
+    parser.add_argument('--weights', nargs='+', type=str, default='runs/train/exp21/weights/best.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='images/detect', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', type=int, default=768, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.15, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.10, help='IOU threshold for NMS')
     parser.add_argument('--device', default='0', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--view-img', action='store_true', help='display results')
     parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
