1caa28ff62110f52c6ae44b594c93932acac38e6
Jaime Gomez-Chacoff <jaime.gomez-chacoff@arcelormittal.com>
2023-02-03 16:25:40 +0100

    onnx model included

--- models/export.py
+++ models/export.py
@@ -24,8 +24,8 @@ if __name__ == '__main__':
     parser.add_argument('--weights', type=str, default='../runs/train/exp2/weights/best.pt', help='weights path')  # from yolov5/models/
     parser.add_argument('--img-size', nargs='+', type=int, default=[768, 768], help='image size')  # height, width
     parser.add_argument('--batch-size', type=int, default=1, help='batch size')
-    parser.add_argument('--dynamic', action='store_true', help='dynamic ONNX axes')
-    parser.add_argument('--grid', action='store_true', help='export Detect() layer grid')
+    parser.add_argument('--dynamic', action='store_true', default=False, help='dynamic ONNX axes')
+    parser.add_argument('--grid', action='store_true', default=True, help='export Detect() layer grid')
     parser.add_argument('--device', default='cpu', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     opt = parser.parse_args()
     opt.img_size *= 2 if len(opt.img_size) == 1 else 1  # expand
